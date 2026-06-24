5e4b9607e0193c4bfb0b4c676da0218bd5c1f9f0
Maxim Brett <maxim.brett.ru@gmail.com>
2026-06-25 01:43:02 +0400

    feat(open-close-princip): add 2 try answer for open-close-principe: add notificationCalculator and delete enum

--- src/main/java/com/hackaton/open_close_principe/good/Main.java
+++ src/main/java/com/hackaton/open_close_principe/good/Main.java
@@ -2,124 +2,151 @@ package com.hackaton.open_close_principe.good;
 
 public class Main {
     public static void main(String[] args) {
-
+        NotificationCalculator notificationCalculator = new NotificationCalculator();
 
         Notification email = new EmailNotification("Welcome to the course!", 1);
-        Notification sms = new SMSNotification( "Your homework was checked", 2);
-        Notification push = new PushNotification( "New lesson is available", 3);
+        Notification sms = new SMSNotification("Your homework was checked", 2);
+        Notification push = new PushNotification("New lesson is available", 3);
         Notification telegram = new TelegramNotification("New pattern", 1);
 
-        printPrice(email);
-        printPrice(sms);
-        printPrice(push);
-        printPrice(telegram);
+        printPrice(email, notificationCalculator);
+        printPrice(sms, notificationCalculator);
+        printPrice(push, notificationCalculator);
+        printPrice(telegram, notificationCalculator);
     }
 
-    private static void printPrice(Notification notification) {
-        System.out.println(notification.getType() + " price: " + notification.calculatePrice());
+    private static void printPrice(Notification notification, NotificationCalculator notificationCalculator) {
+        System.out.println(notification.getType() + " price: " + notificationCalculator.calculatePrice(notification));
     }
 }
 
 
 interface Notification {
-    double calculatePrice();
-    Channel getType();
+    String getType();
+
+    Double getPricePerRecipient();
+
+    Integer getRecipientsCount();
 }
 
-enum Channel {
-    SMS,
-    EMAIL,
-    Push,
-    Telegram
+class NotificationCalculator {
+
+    double calculatePrice(Notification notification) {
+        return notification.getPricePerRecipient() * notification.getRecipientsCount();
+    }
 }
 
 
-class EmailNotification implements Notification {
-    private static final Channel type = Channel.EMAIL;
-    public static final double tax = 0.1;
-    private int recipientsCount;
-    private String message;
+abstract class NotificationData {
+    public int recipientsCount;
+    public String message;
 
-    public EmailNotification(String message, int recipientsCount ) {
+    public NotificationData(String message, int recipientsCount) {
         this.recipientsCount = recipientsCount;
         this.message = message;
     }
 
-    @Override
-    public double calculatePrice() {
-        return tax * recipientsCount;
-    }
-
-    @Override
-    public Channel getType() {
-        return type;
-    }
 
 }
 
-class SMSNotification implements Notification {
-    private static final Channel type = Channel.SMS;
-    public static final double tax = 0.75;
-    private int recipientsCount;
-    private String message;
+class EmailNotification extends NotificationData implements Notification {
+    private static final String type = "Email";
+    private static final double PRICE_PER_RECIPIENT = 0.1;
+
+
+    public EmailNotification(String message, int recipientsCount) {
+        super(message, recipientsCount);
+    }
+
+    @Override
+    public Double getPricePerRecipient() {
+        return PRICE_PER_RECIPIENT;
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public Integer getRecipientsCount() {
+        return recipientsCount;
+    }
+}
+
+class SMSNotification extends NotificationData implements Notification {
+    private static final String type = "SMS";
+    public static final double PRICE_PER_RECIPIENT = 0.75;
+
 
     public SMSNotification(String message, int recipientsCount) {
-        this.recipientsCount = recipientsCount;
-        this.message = message;
+        super(message, recipientsCount);
     }
 
     @Override
-    public double calculatePrice() {
-        return tax * recipientsCount;
+    public Double getPricePerRecipient() {
+        return PRICE_PER_RECIPIENT;
     }
 
     @Override
-    public Channel getType() {
+    public String getType() {
         return type;
+    }
+
+    @Override
+    public Integer getRecipientsCount() {
+        return recipientsCount;
     }
 }
 
-class PushNotification implements Notification {
-    private static final Channel type = Channel.Push;
-    public static final double tax = 0.25;
+class PushNotification extends NotificationData implements Notification {
+    private static final String type = "Push";
+    public static final double PRICE_PER_RECIPIENT = 0.25;
     private int recipientsCount;
     private String message;
 
     public PushNotification(String message, int recipientsCount) {
-        this.recipientsCount = recipientsCount;
-        this.message = message;
+        super(message, recipientsCount);
     }
 
     @Override
-    public double calculatePrice() {
-        return tax * recipientsCount;
+    public Double getPricePerRecipient() {
+        return PRICE_PER_RECIPIENT;
     }
 
     @Override
-    public Channel getType() {
+    public String getType() {
         return type;
+    }
+
+    @Override
+    public Integer getRecipientsCount() {
+        return recipientsCount;
     }
 }
 
-class TelegramNotification implements Notification {
-    private static final Channel type = Channel.Telegram;
-    public static final double tax = 0.4;
-    private int recipientsCount;
-    private String message;
+class TelegramNotification extends NotificationData implements Notification {
+    private static final String type = "Telegram";
+    public static final double PRICE_PER_RECIPIENT = 0.4;
+
 
     public TelegramNotification(String message, int recipientsCount) {
-        this.recipientsCount = recipientsCount;
-        this.message = message;
+        super(message, recipientsCount);
     }
 
     @Override
-    public double calculatePrice() {
-        return tax * recipientsCount;
+    public Double getPricePerRecipient() {
+        return PRICE_PER_RECIPIENT;
     }
 
     @Override
-    public Channel getType() {
+    public String getType() {
         return type;
+    }
+
+    @Override
+    public Integer getRecipientsCount() {
+        return recipientsCount;
     }
 }
 
